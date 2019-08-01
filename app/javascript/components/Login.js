0964438a9448815dc77f4052a7395fb6595b42f7
krupa <krupasuthar147@gmail.com>
2019-08-01 15:03:59 +0530

    login update

--- app/javascript/components/Login.js
+++ app/javascript/components/Login.js
@@ -1,19 +1,24 @@
 import React, { useState, useForceUpdate } from "react"
-import { Container, Form, Button } from 'react-bootstrap'
+import { Container, Form, Button, Row, Col } from 'react-bootstrap'
 import { login} from '../utils/auth_utils'
 import { withRouter } from 'react-router-dom';
+
 function Login(props){
   const [ email, setEmail ] = useState('');
   const [ password, setPassword ] = useState('');
+  const [errors, setErrors] = useState('');
 
   function handleSubmit(event){
     event.preventDefault();
-    login(email, password).then((response) => { props.history.push('/projects') } );
+    login(email, password).then((response) => { props.history.push('/projects') } )
+                          .catch((errors) => {console.log(errors.error); setErrors(errors) });
   }
 
   return(
         <React.Fragment>
-        <div>
+          {/* <Row> */}
+        <div className="d-flex justify-content-md-center">
+          { errors ? (<div>errors</div>) : null }
         <h2>Login</h2>
         <Form onSubmit={handleSubmit}>
           <Form.Group>
@@ -30,6 +35,7 @@ function Login(props){
           </Button>
           </Form>
           </ div>
+          {/* </Row> */}
       </React.Fragment>
   );
 
